885f85315a4c85d45c8a5e437e25963375c27140
Kristy Lee Gogolen <kl@klcreativedesign.com>
2011-12-26 06:58:39 -0700

    Initial Commit

---
+++ altimeter/altimiterAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  altimiterAppDelegate.h
+//  altimeter
+//
+//  Created by Kristylee Gogolen on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class altimiterViewController;
+
+@interface altimiterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) altimiterViewController *viewController;
+
+@end
---
+++ altimeter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  altimeter
+//
+//  Created by Kristylee Gogolen on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "altimiterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([altimiterAppDelegate class]));
+    }
+}
